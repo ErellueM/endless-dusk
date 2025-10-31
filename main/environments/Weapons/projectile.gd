7c59b79ab16159f7fe18d3d79eb56c7333a6d280
Felix Tillmann <inf24225@lehre.dhbw-stuttgart.de>
2025-10-31 01:43:25 +0100

    Schwertwaffen entfernt und Platz für neue Waffenlogik geschaffen
    Funktionierendes Schießen mit Kollision ist möglich und Hitabfrage wurde gemacht

---
+++ main/environments/Weapons/projectile.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+var velocity = Vector2.ZERO
+var speed = 100
+
+func _physics_process(delta):
+	position += velocity * delta
+
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemygroup"):
+		print("Getroffen: ", body.name)
+		queue_free()
--- main/weapons/melee_weapon.gd
+++
@@ -1,3 +0,0 @@
-extends Weapon
-class_name MeleeWeapon
-@export var knockback: int
