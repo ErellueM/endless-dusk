3f680f6402a92e51c84833a5ac029b1102488cfd
Erik Mueller <erik.mueller0912@gmail.com>
2026-01-27 21:52:35 +0100

    Fix errors after merging

--- maps/map_1.gd
+++ maps/map_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var game_ui = $GameUI
-@onready var wave_handler = $WaveHandler
+@onready var wave_handler = $GameManager/WaveHandler
 
 var player: Node2D
 func _ready():
