c0714c19fa9cf8437c92accb85687fad2ae30392
Beric Bayrakal <beric61@icloud.com>
2026-03-24 11:07:57 +0100

    Add Unittests Beric

---
+++ tests/weapons/equipable_weapons/other/chain_lightning/test_chain_lightning.gd
@@ -0,0 +1,23 @@
+extends GdUnitTestSuite
+
+var weapon
+
+func before_test():
+	weapon = preload("res://main/weapons/equipable_weapons/other/chain_lightning/chain_lightning.tscn").instantiate()
+	add_child(weapon)
+	await get_tree().process_frame
+
+func after_test():
+	if weapon and is_instance_valid(weapon):
+		weapon.queue_free()
+
+func test_initial_values():
+	assert_that(weapon.level).is_equal(1)
+
+func test_apply_level_upgrade():
+	weapon.apply_level_upgrade(2)
+	assert_that(weapon.level).is_equal(2)
+
+func test_attack():
+	var result = weapon.attack()
+	assert_that(result).is_true()
